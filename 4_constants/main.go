95fed9a53ba5a71ac10325cc7a7a6286224d59ac
Tapaswalde <tapaswalde@gmail.com>
2026-02-08 00:08:29 +0530

    constants in go

---
+++ 4_constants/main.go
@@ -0,0 +1,16 @@
+package main 
+import "fmt"
+
+const age=30
+
+const(
+	port=5000
+	host="localhost"
+)
+
+func main(){
+
+	const name=" Go "
+	// fmt.Println(name,age,port,host)
+	fmt.Println(name,age,port,host)
+}
